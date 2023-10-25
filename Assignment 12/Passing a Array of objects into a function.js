813048b208f5ac1fab80b1a83cf7aa12fd9a6bad
saibhavithra <saibhavithra@gmail.com>
2023-10-24 21:08:38 -0500

    Passing a array of objects into the function

---
+++ Assignment 12/Passing a Array of objects into a function.js
@@ -0,0 +1,34 @@
+const sales = [
+    {item:'PS4 Pro', stock:3, original:399.99},
+    {item:'Xbox One X', stock:1, original:499.99, discount:0.1},
+    {item:'Nintendo Switch', stock:4, original:299.99},
+    {item:'PS2 Console', stock:1, original:299.99, discount:0.8},
+    {item:'Nintendo 64', stock:2, original:199.99, discount:0.65}
+];
+
+const Sales1= sales.map((element)=>
+
+{if (element.item =="PS4 Pro" || element.item =="Nintendo Switch")
+{  var Total = element.stock * element.original
+    var Item=element.item;
+    var Stock=element.stock;
+    var Original=element.original;
+    var Sale = element.original;
+
+  return{Item,Stock,Original,Sale,Total};
+}
+ else        
+{var  Sale= (element.original - (element.original*element.discount)) 
+    var  Total = element.stock *  Sale             
+    { 
+        var Item=element.item;
+        var Stock=element.stock;
+        var Original=element.original;
+        var Discount=element.discount;
+        
+        return{Item,Stock,Original,Discount,Sale,Total};
+       
+    }     
+} })
+
+console.log(Sales1)
